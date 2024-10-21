a17d344239766e0df582ea68f240f4f2b72a34cd
Miroslav Tsarev <mtzarev@gmail.com>
2024-10-21 21:53:31 +0300

    Add files via upload

---
+++ Input.js
@@ -0,0 +1,15 @@
+//import InputTest from "./components/InputTest"
+function Input (){
+return (
+    <>
+    <h2>Tasks</h2>
+    <ol>
+        <Input text="First Try Props" />
+        <Input text="Second Try Props"/>
+        <Input text="Third Try Props"/>
+
+    </ol>
+    </>
+)
+}
+export default Input
---
+++ InputTest.js
@@ -0,0 +1,5 @@
+let InputTest = (props) => {
+    return<li> {props.text}</li>
+
+}
+export default InputTest
