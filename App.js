e7ff178f9fd5255019b87412432b2340b39605b8
Miroslav Tsarev <mtzarev@gmail.com>
2024-10-21 21:53:13 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Input from './components/Input.js'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit the App.js
+        </p>
+       
+          <main>
+          
+          </main>
+          <Input/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
