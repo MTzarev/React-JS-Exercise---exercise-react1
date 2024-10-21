4659d6f7658b098927230ecdb54d870282d41a67
Miroslav Tsarev <mtzarev@gmail.com>
2024-10-21 21:55:12 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import './App.css';
 import Input from './components/Input.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App">;
+      <header className="App-header">;
+        <img src={logo} className="App-logo" alt="logo" />;
         <p>
           Edit the App.js
         </p>
